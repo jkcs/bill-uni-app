001d22dd319fc9ef018d35fa75727bab74ed9b8a
liuwei <liuwei@woaizuji.com>
2020-12-23 10:27:59 +0800

    :tada: daily code

---
+++ pages/bill/addition.vue
@@ -0,0 +1,107 @@
+<template>
+  <view :class="bem('container')">
+    <scroll-view scroll-y show-scrollbar :class="bem('grid')">
+      <view v-for="(item, index) in list" :key="index" :class="bem('grid__item', { active: item._checked })" @click="handleClickItem(item)">
+        <image :class="bem('grid__item__image')" :src="`/static/icons/keyboard/${item.icon}.png`" model="widthFix"></image>
+        <text :class="bem('grid__item__text')">{{item.tagName}}</text>
+      </view>
+    </scroll-view>
+    <keyboard v-if="false" :date.sync="date" :remark.sync="remark"></keyboard>
+  </view>
+</template>
+
+<script>
+  import { createNameSpace } from '@/common/utils/bem'
+
+  const bem = createNameSpace('addition')
+  export default {
+    data() {
+      return {
+        date: undefined,
+        remark: undefined,
+        list: [
+          { icon: 'date', tagName: '日期', _checked: false },
+          { icon: 'date', tagName: '日期', _checked: false },
+          { icon: 'date', tagName: '日期', _checked: false },
+          { icon: 'date', tagName: '日期', _checked: false },
+          { icon: 'date', tagName: '日期', _checked: false },
+          { icon: 'date', tagName: '日期', _checked: false },
+          { icon: 'date', tagName: '日期', _checked: false },
+          { icon: 'date', tagName: '日期', _checked: false },
+          { icon: 'date', tagName: '日期', _checked: false },
+          { icon: 'date', tagName: '日期', _checked: false },
+          { icon: 'date', tagName: '日期', _checked: false },
+          { icon: 'date', tagName: '日期', _checked: false },
+          { icon: 'date', tagName: '日期', _checked: false },
+          { icon: 'date', tagName: '日期', _checked: false },
+          { icon: 'date', tagName: '日期', _checked: false },
+          { icon: 'date', tagName: '日期', _checked: false },
+          { icon: 'date', tagName: '日期', _checked: false },
+          { icon: 'date', tagName: '日期', _checked: false },
+          { icon: 'date', tagName: '日期', _checked: false },
+          { icon: 'date', tagName: '日期', _checked: false },
+          { icon: 'date', tagName: '日期', _checked: false },
+          { icon: 'date', tagName: '日期', _checked: false },
+          { icon: 'date', tagName: '日期', _checked: false },
+          { icon: 'date', tagName: '日期', _checked: false },
+          { icon: 'date', tagName: '日期', _checked: false },
+          { icon: 'date', tagName: '日期', _checked: false },
+          { icon: 'date', tagName: '日期', _checked: false },
+          { icon: 'date', tagName: '日期', _checked: false },
+          { icon: 'date', tagName: '日期', _checked: false },
+          { icon: 'date', tagName: '日期', _checked: false },
+          { icon: 'date', tagName: '日期', _checked: false },
+          { icon: 'date', tagName: '日期', _checked: false },
+          { icon: 'date', tagName: '日期', _checked: false },
+          { icon: 'date', tagName: '日期', _checked: false }
+        ]
+      }
+    },
+    methods: {
+      bem,
+      handleClickItem(item) {
+        item._checked = true
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .addition{
+    background-color: #fff;
+    &__container {
+      padding: 20rpx 0;
+    }
+    &__grid {
+      display: flex;
+      flex-wrap: wrap;
+      &__item {
+        width: 25%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        padding-bottom: $spacing-row-lg;
+        align-items: center;
+        &--active > &-image {
+          @include main-bg;
+        }
+        &--active > &__text {
+          font-weight: bold;
+        }
+        &__image {
+          width: 110rpx;
+          height: 110rpx;
+          border-radius: 50%;
+        }
+        &__text {
+          padding-top: $spacing-row-base;
+          font-size: $font-size-sm;
+          text-align: center;
+          color: #000;
+          font-weight: 400;
+          line-height: 30rpx;
+        }
+      }
+    }
+  }
+</style>
--- pages/index/list.vue
+++ pages/index/list.vue
@@ -1,45 +1,58 @@
 <template>
   <view :class="bem('container')">
-    <view :class="bem('grid')">
-      <view :class="bem('grid-item')">
-        <image :class="bem('grid-item-image')" src="/static/icons/keyboard/date.png" model="widthFix"></image>
-        <text :class="bem('grid-item-text')">测试</text>
+    <navigator url="bill/addition">
+      <button type="default">跳转到发布页面</button>
+    </navigator>
+    <view :class="bem('header')">
+      <view :class="bem('header-left')">left icon</view>
+      <view :class="bem('header-center')">bill</view>
+    </view>
+    <view :class="bem('count')">
+      <view :class="bem('count-left')">
+        <text :class="bem('count-title')">2020年</text>
+        <text :class="bem('count-content')">11<text :class="bem('count-content__sub')">月</text> <view :class="bem('count-content__triangle')"></view></text>
       </view>
-      <view :class="bem('grid-item', { active: true })">
-        <image :class="bem('grid-item-image')" src="/static/icons/keyboard/backspace.png" model="widthFix"></image>
-        <text :class="bem('grid-item-text')">测试</text>
+      <view :class="bem('count-right')">
+        <text :class="bem('count-title')">收入</text>
+        <text :class="bem('count-content')">0.00</text>
       </view>
-      <view :class="bem('grid-item')">
-        <image :class="bem('grid-item-image')" src="/static/icons/keyboard/date.png" model="widthFix"></image>
-        <text :class="bem('grid-item-text')">测试</text>
-      </view>
-      <view :class="bem('grid-item')">
-        <image :class="bem('grid-item-image')" src="/static/icons/keyboard/date.png" model="widthFix"></image>
-        <text :class="bem('grid-item-text')">测试</text>
-      </view>
-      <view :class="bem('grid-item')">
-        <image :class="bem('grid-item-image')" src="/static/icons/keyboard/date.png" model="widthFix"></image>
-        <text :class="bem('grid-item-text')">测试</text>
-      </view>
-      <view :class="bem('grid-item')">
-        <image :class="bem('grid-item-image')" src="/static/icons/keyboard/date.png" model="widthFix"></image>
-        <text :class="bem('grid-item-text')">测试</text>
+      <view :class="bem('count-right')">
+        <text :class="bem('count-title')">支出</text>
+        <text :class="bem('count-content')">0.00</text>
       </view>
     </view>
-    <keyboard v-if="false" :date.sync="date" :remark.sync="remark"></keyboard>
+    <view :class="bem('list')">
+      <view :class="bem('list__item')">
+        <view :class="bem('list__item-bar')">
+          <view :class="bem('list__item-bar__title')">12月08日</view>
+          <view :class="bem('list__item-bar__right')">
+            <text :class="bem('list__item-bar__right__title')">支出：</text>
+            <text :class="bem('list__item-bar__right__amount')">0.00</text>
+          </view>
+          <view :class="bem('list__item-bar__right')">
+            <text :class="bem('list__item-bar__right__title')">收入：</text>
+            <text :class="bem('list__item-bar__right__amount')">0.00</text>
+          </view>
+        </view>
+        <view :class="bem('list__item-sub')">
+          <view :class="bem('list__item-sub__icon')">
+            <image class="icon icon-m" :src="`/static/icons/keyboard/date.png`" model="widthFix"></image>
+          </view>
+          <view :class="bem('list__item-sub__text')">吃饭</view>
+          <view :class="bem('list__item-sub__amount')">- 2.12</view>
+        </view>
+      </view>
+    </view>
   </view>
 </template>
 
 <script>
-  import { createNameSpace } from "@/common/utils/bem"
+  import { createNameSpace } from '@/common/utils/bem'
 
   const bem = createNameSpace('list')
   export default {
     data() {
-      return {
-        date: undefined,
-        remark: undefined
-      }
+      return {}
     },
     methods: {
       bem
@@ -48,41 +61,64 @@
 </script>
 
 <style lang="scss" scoped>
-.list{
-  background-color: #fff;
-  &__container {
-    padding: 20rpx 0;
-  }
-  &__grid {
-    display: flex;
-    flex-wrap: wrap;
-    &-item {
-      width: 25%;
+  .list {
+    &__container {
+      padding: 15rpx;
+    }
+    &__header {
       display: flex;
-      flex-direction: column;
-      justify-content: center;
-      padding-bottom: $spacing-row-lg;
-      align-items: center;
-      &--active > &-image {
-        @include main-bg;
+      justify-content: space-between;
+      &-left {
+        font-size: $font-size-s;
       }
-      &--active > &-text {
-        font-weight: bold;
+      &-center {
+        align-self: center;
+        font-size: $font-size-base;
       }
-      &-image {
-        width: 110rpx;
-        height: 110rpx;
-        border-radius: 50%;
+    }
+    &__count {
+      &-left {
+
       }
-      &-text {
-        padding-top: $spacing-row-base;
-        font-size: $font-size-sm;
-        text-align: center;
-        color: #000;
-        font-weight: 400;
-        line-height: 30rpx;
+      &-right {
+
+      }
+      &-title {
+
+      }
+      &-content {
+
+      }
+    }
+    &__list {
+      &__item {
+        &-bar {
+          &__title {
+
+          }
+          &__right {
+            &__title {
+
+            }
+            &__amount {
+
+            }
+          }
+        }
+
+        &-sub {
+          &__icon {
+
+          }
+          &__text {
+
+          }
+          &__amount {
+
+          }
+        }
       }
     }
   }
-}
+
 </style>
